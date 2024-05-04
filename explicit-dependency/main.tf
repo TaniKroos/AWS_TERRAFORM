7facf17bc78ff8adcbc8a40d00349fb9d998d651
TaniKroos <tanishsaini26@gmail.com>
2024-05-04 13:27:35 +0530

    :)

--- explicit-dependency/main.tf
+++ explicit-dependency/main.tf
@@ -1,9 +1,9 @@
 resource "local_file" "whale" {
-  filename = "/Users/kroos31/Desktop/linux/terraform/explicit-dependency/whale.txt"
-  content = "whale"
-  depends_on = [ local_file.krill ]
+  filename   = "/Users/kroos31/Desktop/linux/terraform/explicit-dependency/whale.txt"
+  content    = "whale"
+  depends_on = [local_file.krill]
 }
 resource "local_file" "krill" {
   filename = "/Users/kroos31/Desktop/linux/terraform/explicit-dependency/krill.txt"
-  content = "krill"
+  content  = "krill"
 }
