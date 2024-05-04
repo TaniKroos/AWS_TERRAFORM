0eebea51ae2667c3acffcc429ed06d8afef4ea46
TaniKroos <tanishsaini26@gmail.com>
2024-05-04 18:22:38 +0530

    DynamoDB

--- dynamodb/main.tf
+++ dynamodb/main.tf
@@ -8,3 +8,12 @@ resource "aws_dynamodb_table" "Pixars" {
     }
   
 }
+resource "aws_dynamodb_table_item" "upload" {
+  table_name = aws_dynamodb_table.Pixars.name
+  hash_key   = aws_dynamodb_table.Pixars.hash_key
+  item = <<EOF
+ {
+   "MIn": {"N": "1"}
+}
+EOF
+}
