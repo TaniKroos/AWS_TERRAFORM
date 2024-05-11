c62117c28a429d033319773a5953ccd1a9b51714
TaniKroos <tanishsaini26@gmail.com>
2024-05-11 14:33:24 +0530

    EBS

---
+++ ebs/main.tf
@@ -0,0 +1,20 @@
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.web.id
+}
+
+resource "aws_instance" "web" {
+  ami               = "ami-023e152801ee4846a"
+  availability_zone = "us-west-2a"
+  instance_type     = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-west-2a"
+  size              = 1
+}
