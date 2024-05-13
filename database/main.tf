b14eb4b2e22cfa253a050af4521df71270012ce9
TaniKroos <tanishsaini26@gmail.com>
2024-05-13 13:02:19 +0530

    RDS_AWS

--- database/main.tf
+++ database/main.tf
@@ -4,8 +4,8 @@ resource "aws_db_instance" "db" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
-  username             = "foo"
-  password             = "foobarbaz"
+  username             = ""
+  password             = ""
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 
